320105649fe2ef3f161e136c69f4e8220586a3ee
RyanHPaez <solosnail@gmail.com>
2022-02-07 10:06:29 -0800

    Update index.js

--- JS-Callback-Practice/index.js
+++ JS-Callback-Practice/index.js
@@ -1,10 +1,28 @@
 const inventory = newInventory()
 move(inventory).to(0, 0)
 
-const character = newImage('assets/green-character/static.gif')
-move(character).to(100, 250)
-
-
+const character = newImage('assets/green-character/static.gif');
+//Changes character's appearance based on direction
+function handleDirectionChange(direction){
+    switch(direction){
+        case 'null':
+            character.src = 'assets/green-character/static.gif'
+        break;
+        case 'north':
+            character.src = 'assets/green-character/north.gif'
+        break;
+        case 'south':
+            character.src = 'assets/green-character/south.gif'
+        break;
+        case 'east':
+            character.src = 'assets/green-character/east.gif'
+        break;
+        case 'west':
+            character.src = 'assets/green-character/west.gif'
+        break;
+    }
+}
+move(character).withArrowKeys(100, 250, handleDirectionChange);
 move(newImage('assets/tree.png')).to(200, 450)
 move(newImage('assets/pillar.png')).to(350, 250)
 move(newImage('assets/pine-tree.png')).to(450, 350)
@@ -13,3 +31,5 @@ move(newImage('assets/well.png')).to(500, 575)
 move(newItem('assets/sword.png')).to(500, 555)
 move(newItem('assets/shield.png')).to(165, 335)
 move(newItem('assets/staff.png')).to(600, 250)
+
+
